fe468eed4eb3c954591bd6a77c3f2f6823e1151b
Zamudio Alejandro <ale-zamudio2011@hotmail.com>
2022-08-07 00:11:27 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs-module.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent  {
+
+
+ // CAMBIAR AÑO AUTOMÁTICAMENTE
+//fecha(){
+
+//  const fecha: number = new Date();
+//  const anio = fecha.getFullYear();
+
+ // this.fecha = new Date();
+  //anio = this.fecha.getFullYear();
+  //this.contenedorAno.innerHTML = this.anio 
+//}
+
+}
